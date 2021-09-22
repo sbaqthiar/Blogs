1fe0e4eacc774b5f76d329413b19cd0b66378bc6
sbaqthiar <sbaqthiar@gmail.com>
2021-09-22 21:07:08 +0530

    updated component NavButtons

--- src/NavButtons.js
+++ src/NavButtons.js
@@ -1,14 +1,22 @@
 import React from 'react'
+import {useHistory} from 'react-router-dom'
+import { useEffect } from 'react'
 
 function NavButtons() {
+    
+        const hist = useHistory();
+    
     return (
         <>
             <div className="row">
                 <nav>
-                    <a href="/"><button className="btn btn-primary button">Dashboard</button></a>
+                    {/* <a href="/"><button className="btn btn-primary button">Dashboard</button></a>
                     <a href="/PostsPage"><button className="btn btn-primary button">Posts Page</button></a>
-                    <a href="/LinksPage"><button className="btn btn-primary button">Links Page</button></a>
+                    <a href="/LinksPage"><button className="btn btn-primary button">Links Page</button></a> */}
                     
+                    <button className="btn btn-primary button" onClick={()=> hist.push('/') }>Dashboard</button>
+                    <button className="btn btn-primary button" onClick={()=> hist.push('/PostsPage') }>Posts Page</button>
+                    <button className="btn btn-primary button" onClick={()=> hist.push('/LinksPage') }>Links Page</button>
                 </nav>
             </div>
         </>
